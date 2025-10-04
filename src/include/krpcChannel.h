a70fe847ecf47620dfbb7631fe6d519470fd33eb
zhangyan <13682105169@163.com>
2025-10-04 22:31:54 +0800

    update

--- src/include/krpcChannel.h
+++ src/include/krpcChannel.h
@@ -11,31 +11,41 @@ class KrpcChannel : public google::protobuf::RpcChannel // 继承google::protobu
 {
 public:
     KrpcChannel(bool connectNow);
-    virtual ~KrpcChannel()
-    {
+    virtual ~KrpcChannel() { }
 
-    }
+    /*
+    * 核心方法：CallMethod()，实现 Protobuf 框架定义的虚函数 CallMethod()
+    * stub 代理类在调用远程方法时，最终都会调用到此函数，统一做 rpc 方法调用的数据序列化和网络发送。
+    * 它负责：
+    * 1. 从 ZooKeeper 查询服务地址
+    * 2. 建立 Socket 连接
+    * 3. 将请求序列化，并发送给服务端
+    * 4. 接收响应并反序列化，并返回结果
+    */
 
-    // 重写RpcChannel中的CallMethod()
-
-    // 所有通过 stub 代理对象调用的 rpc 方法，都会走到这里，统一做 rpc 方法调用的数据序列化和网络发送
+    // RPC 调用的核心方法，负责将客户端的请求序列化并发送到服务端，同时接收服务端的响应
     void CallMethod(const ::google::protobuf::MethodDescriptor* method, // 调用哪个服务、哪个方法
-                    ::google::protobuf::RpcController* controller,      // 上下文控制器
-                    const ::google::protobuf::Message* request,         // 请求参数
-                    ::google::protobuf::Message* response,              // 请求响应
-                    ::google::protobuf::Closure* done) override;        // 回调
+                    ::google::protobuf::RpcController* controller, // 上下文控制器
+                    const ::google::protobuf::Message* request,    // 请求参数
+                    ::google::protobuf::Message* response,         // 请求响应
+                    ::google::protobuf::Closure* done) override;   // 回调
 
 
 private:
-    int m_clientfd; // 存放客户端sockfd
-    std::string service_name;
-    std::string m_ip;
-    uint16_t m_port;
-    std::string method_name;
+    int m_clientfd;             // 当前客户端sockfd
 
-    int m_idx; // 划分服务器ip和port的下标
+    std::string service_name;   // 当前调用的RPC服务名称，如："UserService"
+    std::string method_name;    // 当前调用的服务方法名称，如："Login" "Register"
+    
+    std::string m_ip;           // 从zookeeper获取的服务端 ip
+    uint16_t m_port;            // 从zookeeper获取的服务端 port
+    
+    int m_idx; // // 字符串中':'分隔符的位置，划分服务器ip和port的下标
+
+    // 创建新的socket连接
     bool newConnect(const char* ip, uint16_t port);
 
+    // 从ZooKeeper查询指定服务方法的服务端地址 ip:port
     std::string QueryServiceHost(ZkClient* zkclient, std::string service_name, std::string method_name, int& idx);
 };
 
---
+++ src/include/krpcProvider.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "google/protobuf/service.h"
+#include "zookeeperutil.h"
+
+#include <muduo/net/TcpServer.h>
+#include <muduo/net/EventLoop.h>
+#include <muduo/net/InetAddress.h>
+#include <muduo/net/TcpConnection.h>
+#include <google/protobuf/descriptor.h>
+#include <functional>
+#include <string>
+#include <unordered_map>
+
+
+class KrpcProvider
+{
+public:
+    // 这里是提供给外部使用的，可以发布 RPC方法 的函数接口
+    void NotifyService(google::protobuf::Service* service);
+    ~KrpcProvider();
+
+    // 启动RPC服务节点，开始提供RPC远程网络调用服务
+    void Run();
+
+private:
+    muduo::net::EventLoop event_loop;
+    struct ServiseInfo
+    {
+        google::protobuf::Service* service;
+        std::unordered_map<std::string, const google::protobuf::MethodDescriptor*> method_map;
+    };
+
+    std::unordered_map<std::string, ServiseInfo> service_map; // 保存服务对象和RPC方法
+
+    void OnConnection(const muduo::net::TcpConnectionPtr& conn);
+    void OnMessage(const muduo::net::TcpConnectionPtr& conn, muduo::net::Buffer* buffer, muduo::Timestamp receive_time);
+    void SendRpcResponse(const muduo::net::TcpConnectionPtr& conn, google::protobuf::Message* response);
+};
